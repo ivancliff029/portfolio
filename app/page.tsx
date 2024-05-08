32b406687e5bc8cc9333be75fd233571b55c6026
ivancliff029 <odekeivancliff@gmail.com>
2024-05-08 15:42:17 +0300

    fixing the apostrophe errror

--- app/page.tsx
+++ app/page.tsx
@@ -72,16 +72,18 @@ const AboutMe = () => {
 const ContactForm = () => {
   return (
   <>
-    <section className="contact-form flex flex-col items-center justify-center h-screen bg-white text-gray-800">
-      <h2 className="text-2xl font-bold mb-4">Contact Me</h2>
-      <div className="flex flex-col items-center justify-center">
-        <p className="mb-2"><strong>Email:</strong> odekeivancliff@gmail.com</p>
-        <p className="mb-2"><strong>Phone:</strong> +256778054598</p>
-      </div>
-      <button className="bg-blue-500 text-white py-2 px-4 rounded-full font-semibold cursor-pointer mt-4">
-        Send Message
-      </button>
-    </section>
+
+      <section className="contact-form flex flex-col items-center justify-center h-screen bg-white text-gray-800">
+        <h2 className="text-2xl font-bold mb-4">Contact Me</h2>
+        <div className="flex flex-col items-center justify-center">
+          <p className="mb-2"><strong>Email:</strong> odekeivancliff@gmail.com</p>
+          <p className="mb-2"><strong>Phone:</strong> +256778054598</p>
+        </div>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded-full font-semibold cursor-pointer mt-4">
+          Send Message
+        </button>
+      </section>
+    
   </>
     
   );
