23ccc4f4e38b586722481977a2074d0987372856
ivancliff029 <odekeivancliff@gmail.com>
2024-07-24 12:41:28 +0300

    Fix:load animation for blogs

--- app/Components/Spinner.tsx
+++ app/Components/Spinner.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import './spinner.css';
 
 const Spinner: React.FC = () => (
-  <div className="flex items-center justify-center h-screen">
-    <h2 className="text-2xl font-bold mb-4">Loading Blogs</h2><br />
-    <div className="w-16 h-16 border-4 border-t-4 border-blue-500 border-solid rounded-full animate-spin">
-        
-    </div>
+  <div className="flex flex-col items-center justify-center h-screen">
+    <h2 className="text-2xl font-semibold mb-4 text-gray-700">Loading Blogs...</h2>
+    <div className="w-16 h-16 border-4 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
   </div>
 );
 
