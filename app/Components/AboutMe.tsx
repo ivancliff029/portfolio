ede1af5118c51225a2657bf134efcc54b9d4983a
ivancliff029 <odekeivancliff@gmail.com>
2024-08-12 10:42:10 +0300

    Update: updated some few texts

--- app/Components/AboutMe.tsx
+++ app/Components/AboutMe.tsx
@@ -38,13 +38,13 @@ const AboutMe = () => {
           initial={{ x: 100, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.8, delay: 0.4 }}
-          className="md:w-2/3 bg-white rounded-lg shadow-md p-8"
+          className="md:w-2/3 bg-white dark:bg-gray-800 rounded-lg shadow-md p-8"
         >
           <motion.h2
             initial={{ y: -20, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.6 }}
-            className="text-3xl font-bold mb-4"
+            className="text-3xl font-bold mb-4 text-gray-900 dark:text-gray-100"
           >
             About Odeke
           </motion.h2>
@@ -52,37 +52,10 @@ const AboutMe = () => {
             initial={{ y: 20, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.8 }}
-            className="text-lg mb-4"
+            className="text-lg mb-4 text-gray-800 dark:text-gray-200"
           >
-            I am Odeke Ivan, a Computer Scientist focusing my research in AI and IOT technology. With over 5 years of experience in coding, I'm a senior developer dedicated to creating innovative solutions. I approach project development with an open mind and a commitment to excellence.
+            I am Odeke Ivan, a Computer Scientist focusing my research in AI and IoT technology. I am a developer with over 8 years of programming, I'm dedicated to creating innovative solutions and I approach project development with an open mind and a commitment to excellence.
           </motion.p>
-          <motion.div
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="mt-4"
-          >
-            <motion.p
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.8 }}
-            className="text-lg mb-4"
-          >
-            <b>Badges</b>
-          </motion.p>
-            <a 
-              href="https://tryhackme.com/p/ivancliff029" 
-              target="_blank" 
-              rel="noopener noreferrer"
-              className="inline-block transition-transform duration-300 ease-in-out hover:scale-105"
-            >
-              <img 
-                src="https://tryhackme-badges.s3.amazonaws.com/ivancliff029.png" 
-                alt="TryHackMe Badge" 
-                className="w-full max-w-[300px] h-auto"
-              />
-            </a>
-          </motion.div>
         </motion.div>
       </motion.div>
     </div>
