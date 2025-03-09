99396c35fe8254f45d3c138dba408ec3686db8f3
ivancliff029 <odekeivancliff@gmail.com>
2025-03-09 22:06:28 +0300

    Update:Buy coffee route

--- app/buy-me-coffee/page.js
+++ app/buy-me-coffee/page.js
@@ -56,7 +56,7 @@ const BuyMeCoffee = () => {
       />
       <div className="flex justify-end space-x-2">
         <button
-          onClick
+          onClick={()=>window.history.back()}
           className="px-4 py-2 bg-gray-300 dark:bg-gray-600 rounded hover:bg-gray-400 dark:hover:bg-gray-500 text-gray-800 dark:text-gray-100"
         >
           Cancel
